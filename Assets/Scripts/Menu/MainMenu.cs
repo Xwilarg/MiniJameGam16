18e5441905300b2b1105c9f1f23b952e3abc4e4f
Xwilarg <xwilarg@protonmail.com>
2023-04-01 16:08:59 +0200

    Add main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MiniJamGame16.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
