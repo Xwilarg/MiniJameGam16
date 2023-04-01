90d8292637411890055011dd52d524612d627d80
Xwilarg <xwilarg@protonmail.com>
2023-04-01 17:52:11 +0200

    Add inspection gameplay

--- Assets/Scripts/Minigame/MinigameManager.cs
+++ Assets/Scripts/Minigame/MinigameManager.cs
@@ -1,6 +1,8 @@
-﻿using MiniJamGame16.Tutorial;
+﻿using MiniJamGame16.Translation;
+using MiniJamGame16.Tutorial;
 using System;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -16,6 +18,12 @@ namespace MiniJamGame16.Minigame
         [SerializeField]
         private GameObject _floxbutton;
 
+        [SerializeField]
+        private TMP_Text _inspectionText;
+        private int _inspectionPercent;
+
+        public bool IsInspectionOn { private set; get; }
+
         public bool IsActive { private set; get; }
 
         public Action _onDone;
@@ -23,6 +31,8 @@ namespace MiniJamGame16.Minigame
         private void Awake()
         {
             Instance = this;
+            _inspectionText.text = $"{Translate.Instance.Tr("INSPECTIONCHANCES").ToUpperInvariant()} 0%";
+            _inspectionText.color = Color.white;
             foreach (var game in _minigames)
             {
                 game.Minigame.OnDone += (_sender, _e) =>
@@ -32,6 +42,10 @@ namespace MiniJamGame16.Minigame
                     _floxbutton.SetActive(false);
                     TutorialManager.Instance.AdvanceTutorial(TutorialAdvancement.GAME_END);
                     _onDone();
+                    _inspectionPercent += 10;
+                    if (_inspectionPercent > 100) _inspectionPercent = 100;
+                    _inspectionText.text = $"{Translate.Instance.Tr("INSPECTIONCHANCES").ToUpperInvariant()} {_inspectionPercent}%";
+                    _inspectionText.color = Color.white;
                 };
             }
         }
@@ -43,7 +57,13 @@ namespace MiniJamGame16.Minigame
             Assert.IsNotNull(target);
             target.Minigame.gameObject.SetActive(true);
             target.Minigame.Init();
-            _floxbutton.SetActive(true);
+            IsInspectionOn = UnityEngine.Random.Range(0, 100) < _inspectionPercent;
+            _floxbutton.SetActive(!IsInspectionOn);
+            if (IsInspectionOn)
+            {
+                _inspectionText.text = Translate.Instance.Tr("INSPECTIONON");
+                _inspectionText.color = Color.red;
+            }
             IsActive = true;
         }
 
@@ -57,6 +77,10 @@ namespace MiniJamGame16.Minigame
             }
             TutorialManager.Instance.AdvanceTutorial(TutorialAdvancement.GAME_END);
             _onDone();
+            _inspectionPercent -= 50;
+            if (_inspectionPercent < 0) _inspectionPercent = 0;
+            _inspectionText.text = $"{Translate.Instance.Tr("INSPECTIONCHANCES").ToUpperInvariant()} {_inspectionPercent}%";
+            _inspectionText.color = Color.white;
         }
     }
 }
