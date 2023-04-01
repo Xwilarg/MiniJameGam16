f830cc60ce133f1560388f4d2578fd280941a240
Xwilarg <xwilarg@protonmail.com>
2023-04-02 01:25:35 +0200

    Begin minesweeper

---
+++ Assets/Scripts/Minigame/Impl/MineData.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine.UI;
+
+namespace MiniJamGame16.Minigame.Impl
+{
+    public class MineData
+    {
+        public TMP_Text Text { set; get; }
+        public Button Button { set; get; }
+        public int Value { set; get; }
+        public bool IsMine { set; get; }
+    }
+}
