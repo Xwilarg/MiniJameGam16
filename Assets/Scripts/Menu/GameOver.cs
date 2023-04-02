5a3d5ea26ace23e0fd581c584f4bd539357c04b4
Xwilarg <xwilarg@protonmail.com>
2023-04-02 17:01:31 +0200

    Add gameover audio

--- Assets/Scripts/Menu/GameOver.cs
+++ Assets/Scripts/Menu/GameOver.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿using MiniJamGame16.Systems;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace MiniJamGame16.Menu
 {
     public class GameOver : MonoBehaviour
     {
+        [SerializeField] private AudioClip _bgm;
+
+        private void Start()
+        {
+            AudioSystem.Instance.PlayMusic(_bgm);
+        }
+
         public void LoadMenu()
         {
             SceneManager.LoadScene("MainMenu");
