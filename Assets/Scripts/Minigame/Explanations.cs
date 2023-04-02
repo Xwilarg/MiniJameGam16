2dc2f76794e1b0303edd4a9028527dd419af4e64
Xwilarg <xwilarg@protonmail.com>
2023-04-02 13:06:35 +0200

    Add help panel

---
+++ Assets/Scripts/Minigame/Explanations.cs
@@ -0,0 +1,17 @@
+﻿using MiniJamGame16.Translation;
+using TMPro;
+using UnityEngine;
+
+namespace MiniJamGame16.Minigame
+{
+    public class Explanations : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _text;
+
+        public void SetExplanations(string s)
+        {
+            _text.text = Translate.Instance.Tr(s);
+        }
+    }
+}
