b49d4cf6db2d6d47735e99a0e889c85097de8ead
Xwilarg <xwilarg@protonmail.com>
2023-04-02 17:08:36 +0200

    Display score in gameover

--- Assets/Scripts/Menu/GameOver.cs
+++ Assets/Scripts/Menu/GameOver.cs
@@ -1,4 +1,6 @@
 ﻿using MiniJamGame16.Systems;
+using MiniJamGame16.Translation;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,10 +9,12 @@ namespace MiniJamGame16.Menu
     public class GameOver : MonoBehaviour
     {
         [SerializeField] private AudioClip _bgm;
+        [SerializeField] private TMP_Text _score;
 
         private void Start()
         {
             AudioSystem.Instance.PlayMusic(_bgm);
+            _score.text = $"{Translate.Instance.Tr("SCORE")} {GlobalOptions.Instance.Score}";
         }
 
         public void LoadMenu()
